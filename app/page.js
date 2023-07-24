578eb52cb5e8c18923e78481b3d53356121db4a3
mahmoudElsawah123 <mahmoudelsawah21@gmail.com>
2023-07-24 05:29:43 +0300

    s

--- app/page.js
+++ app/page.js
@@ -1,76 +1,56 @@
-import dynamic from 'next/dynamic'
-import { getArticleData, getCategoryData } from './lib/DataFetching';
-import { Suspense } from 'react';
+"use client"
+// import dynamic from 'next/dynamic'
+// import { getArticleData, getCategoryData } from './lib/DataFetching';
+import { Suspense, useEffect, useState } from 'react';
 import Loading from './loading';
+import HomeCarousel from './components/HomeCarousel/HomeCarousel';
 
-const AboutUs = dynamic(() => import('./components/home-page/AboutUs'), {
-  ssr: true,
-})
-const Service = dynamic(() => import('./components/Service/Service'), {
-  ssr: true,
-})
-const Goals = dynamic(() => import('./components/Goals/Goals'), {
-  ssr: true,
-})
-const LatestNews = dynamic(() => import('./components/latestNews/LatestNews'), {
-  ssr: true,
-})
-const InstagramBanner = dynamic(() => import('./components/InstagramBanner/InstagramBanner'), {
-  ssr: true,
-})
-const HomeCarousel = dynamic(() => import('./components/HomeCarousel/HomeCarousel'), {
-  ssr: true,
-})
+// const AboutUs = dynamic(() => import('./components/home-page/AboutUs'), {
+//   ssr: true,
+// })
+// const Service = dynamic(() => import('./components/Service/Service'), {
+//   ssr: true,
+// })
+// const Goals = dynamic(() => import('./components/Goals/Goals'), {
+//   ssr: true,
+// })
+// const LatestNews = dynamic(() => import('./components/latestNews/LatestNews'), {
+//   ssr: true,
+// })
+// const InstagramBanner = dynamic(() => import('./components/InstagramBanner/InstagramBanner'), {
+//   ssr: true,
+// })
+// const HomeCarousel = dynamic(() => import('./components/HomeCarousel/HomeCarousel'), {
+//   ssr: true,
+// })
 
 // const DevaStudioVideo = dynamic(() => import('./components/DevaStudioVideo/DevaStudioVideo'), {
 //   ssr: true,
 // })
-const Category = dynamic(() => import('./components/Category/Category'), {
-  ssr: true,
-})
-const Distinguishes = dynamic(() => import('./components/Distinguishes/Distinguishes'), {
-  ssr: true,
-})
+// const Category = dynamic(() => import('./components/Category/Category'), {
+//   ssr: true,
+// })
+// const Distinguishes = dynamic(() => import('./components/Distinguishes/Distinguishes'), {
+//   ssr: true,
+// })
 
 
 
 
 
-export default async function Home () {
-  const data = await getArticleData();
-  const CategoryData = await getCategoryData();
+export default  function Home () {
+  // const data = await getArticleData();
+  // const CategoryData = await getCategoryData();
 
+  const [data , setData] = useState(true);
+  useEffect(()=>{
+    setData(false)
+  },[])
    
   return (
-         <>
-             <Suspense fallback={<Loading/>}>
-             <HomeCarousel/>
-               <section  className='about-us'>
-                <AboutUs/>
-            </section>
-             <section >
-               <Service/>
-             </section>
-             <section >
-               <Goals/>
-            </section>
-            {/* <section>
-               <DevaStudioVideo/>
-            </section> */}
-            <section>
-              <Category CategoryData={CategoryData.data}/>
-            </section>
-            <section>
-              <Distinguishes/> 
-            </section>
-           <section>
-            <LatestNews data={data}/>
-           </section>
-            <section className='instagram-banner'>
-              <InstagramBanner/>
-            </section>
-             </Suspense>
-         </>     
+             <>
+              {data ? <Loading/> : <HomeCarousel/> }
+             </>
   )
 }
 
