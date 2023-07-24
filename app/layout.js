a57872e6c3319229c0eb74a5bd52cbdcede9e762
mahmoudElsawah123 <mahmoudelsawah21@gmail.com>
2023-07-24 17:17:19 +0300

    7

--- app/layout.js
+++ app/layout.js
@@ -75,7 +75,7 @@ export default function RootLayout({ children }) {
               {children}
         </main>
         <footer>
-          <Footer/>
+          {/* <Footer/> */}
           <SocialFooter/>
         </footer>
         </MuiTheme>
